ceaaa92fbeef713d209071df1ee09d55cc909978
JoscaWij <Josca.W@gmx.net>
2020-08-11 14:17:56 +0200

    Alert in handleClick

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,21 +3,81 @@ import React from "react";
 import Square from "./Square";
 
 export default function Board() {
+  const [squares, setSquares] = React.useState([
+    "O",
+    null,
+    "X",
+    "X",
+    "X",
+    "O",
+    "O",
+    null,
+    null,
+  ]);
   const status = "Next player: X";
+
+  function handleClick() {
+    alert("click");
+  }
 
   return (
     <div>
       <div className="status">{status}</div>
       <div className="board">
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
+        <Square
+          value={squares[0]}
+          onClick={function () {
+            setSquares(handleClick);
+          }}
+        />
+        <Square
+          value={squares[1]}
+          onClick={function () {
+            setSquares(handleClick);
+          }}
+        />
+        <Square
+          value={squares[2]}
+          onClick={function () {
+            setSquares(handleClick);
+          }}
+        />
+        <Square
+          value={squares[3]}
+          onClick={function () {
+            setSquares(handleClick);
+          }}
+        />
+        <Square
+          value={squares[4]}
+          onClick={function () {
+            setSquares(handleClick);
+          }}
+        />
+        <Square
+          value={squares[5]}
+          onClick={function () {
+            setSquares(handleClick);
+          }}
+        />
+        <Square
+          value={squares[6]}
+          onClick={function () {
+            setSquares(handleClick);
+          }}
+        />
+        <Square
+          value={squares[7]}
+          onClick={function () {
+            setSquares(handleClick);
+          }}
+        />
+        <Square
+          value={squares[8]}
+          onClick={function () {
+            setSquares(handleClick);
+          }}
+        />
       </div>
     </div>
   );
